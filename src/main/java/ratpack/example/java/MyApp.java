7f057074b2c05d400ddb240f4303471c83ab3c1f
Frank Heinen <frank.heinen@scoop-software.de>
2019-08-20 16:35:45 +0200

    test pipeline

--- src/main/java/ratpack/example/java/MyApp.java
+++ src/main/java/ratpack/example/java/MyApp.java
@@ -1,13 +1,9 @@
 package ratpack.example.java;
 
 import com.datastax.driver.core.Cluster;
-import com.google.common.util.concurrent.Futures;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.netty.handler.codec.http.HttpResponseStatus;
-import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subjects.PublishSubject;
 import org.apache.spark.SparkConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,195 +13,29 @@ import ratpack.handling.Chain;
 import ratpack.handling.Context;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
-/*
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
-*/
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 
-//import java.util.concurrent.Flow;
-//import java.util.concurrent.SubmissionPublisher;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 
 public class MyApp {
     static Logger logger = LoggerFactory.getLogger(MyApp.class.getName());
 
-    static class Todo {
-        private String name;
-
-        public Todo(String name){
-            this.name=name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-
-        public static List<Todo> getTodos(){
-
-            return Arrays.asList(new Todo("eins"), new Todo("zwei"));
-        }
-    }
-    static class ComputeFunction {
-        public static void compute(Integer v) {
-            try {
-                logger.debug("compute integer v: " + v);
-                Thread.sleep(0);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public static void compute(String v) {
-            try {
-                logger.debug("compute String v: " + v);
-                Thread.sleep(0);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        public static void compute(Todo v) {
-            try {
-                logger.debug("compute Todo v: " + v);
-                Thread.sleep(0);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-/*
-    public static class EventListener {
-
-        int count = 0;
-        FluxSink<String> sink;
-
-        void generate() {
-            while (count < 1000) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                sink.next("event " + count);
-                count++;
-            }
-            count++;
-        }
-
-        public void register(FluxSink<String> sink) {
-            this.sink = sink;
-        }
-    }
-*/
     public static void main(String[] args) throws Exception {
+
         logger.info("XX\nXXXXX");
-/*
-        List<String> items = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
-        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
-        publisher.subscribe(new MySubscriber<>());
-
-        items.forEach(s -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            publisher.submit(s);
-        });
-
-        publisher.close();
-
-        List<Optional<String>> listOfOptionals = Arrays.asList(
-                Optional.empty(), Optional.of("foo"), Optional.empty(), Optional.of("bar"));
-
-        List<String> filteredList = listOfOptionals.stream()
-                .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
-                .collect(Collectors.toList());
-
-        List<String> filteredList2 = listOfOptionals.stream()
-                .flatMap(Optional::stream)
-                .collect(Collectors.toList());
-
-
-        Flux<String> dynamicFlux = Flux.create(sink -> {
-            EventListener eventListener = new EventListener();
-            eventListener.register(sink);
-            eventListener.generate();
-        });
-
-        Flux.range(1, 1000)
-                .parallel(8)
-                .runOn(Schedulers.parallel())
-                .subscribe(i -> System.out.println(i));
-
-        dynamicFlux.publishOn(Schedulers.parallel()).parallel(8);
-
-        dynamicFlux.subscribe(System.out::println
-            ,error -> System.err.println("Error " + error)
-            ,() -> System.out.println("Done")
-        //    , sub -> sub.request(13)
-        );
-
-        Flux<Integer> ints = Flux.range(1, 14);
-        ints.subscribe(i -> System.out.println(i),
-                error -> System.err.println("Error " + error),
-                () -> System.out.println("Done"),
-                sub -> sub.request(100));
-*/
-
-/*
-        Observable.range(1, 1_000_000)
-                .observeOn(Schedulers.computation())
-                .subscribe(ComputeFunction::compute);
-*/
-        PublishSubject<Integer> source = PublishSubject.<Integer>create();
-
-        source.observeOn(Schedulers.computation())
-                .subscribe(ComputeFunction::compute, Throwable::printStackTrace);
-
-        IntStream.range(1, 1_000).forEach(source::onNext);
-        Observable.fromFuture(CompletableFuture.completedFuture("test"))
-        .subscribe(ComputeFunction::compute);
-
-        Observable<Todo> vodo= Observable.create(emitter -> {
-            try {
-                List<Todo> todos = Todo.getTodos();
-                for (Todo todo : todos) {
-                    emitter.onNext(todo);
-                }
-                emitter.onComplete();
-            } catch (Exception e) {
-                emitter.onError(e);
-            }
-        });
-        vodo.subscribeOn(Schedulers.single()).subscribe(todo -> System.out.println(todo.toString()));
         String test= System.getenv("CASSANDRA_SEED");
         final String seed= (test!=null && !test.isEmpty())?test:"localhost";
         Cluster.Builder builder = Cluster.builder();
-        builder.withCredentials("trex".trim(), "trex".trim());
+        builder.withCredentials("cassandra".trim(), "cassandra".trim());
         builder.addContactPoint(seed);
         Cluster cluster= builder.build();
 
         SparkConf conf = new SparkConf(true)
                 .set("spark.cassandra.connection.host", seed)
                 .set("spark.cassandra.connection.port", "9042")
-                .set("spark.cassandra.auth.username", "trex")
-                .set("spark.cassandra.auth.password", "trex")
+                .set("spark.cassandra.auth.username", "cassandra")
+                .set("spark.cassandra.auth.password", "cassandra")
                 .set("deploy-mode" ,"client")
                 .set("spark.executor.memory", "6G")
                 .set("spark.driver.memory", "14G")
@@ -261,32 +91,4 @@ public class MyApp {
         }
 
     }
-/*
-    public static class MySubscriber<T> implements Flow.Subscriber<T> {
-
-        private Flow.Subscription subscription;
-
-        @Override
-        public void onSubscribe(Flow.Subscription subscription) {
-            this.subscription = subscription;
-            this.subscription.request(1); //  Ask for initial one data object.
-        }
-
-        @Override
-        public void onNext(T item) {
-            System.out.println(item); // Print it.
-            subscription.request(1); // Ask for one more.
-        }
-
-        @Override
-        public void onError(Throwable throwable) {
-            throwable.printStackTrace();
-        }
-
-        @Override
-        public void onComplete() {
-            System.out.println("DONE"); // Done with the stream of data.
-        }
-    }
-*/
 }
--- src/main/java/ratpack/example/java/MyHandler.java
+++ src/main/java/ratpack/example/java/MyHandler.java
@@ -1,26 +1,64 @@
 package ratpack.example.java;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.ResultSetFuture;
-import com.datastax.driver.core.Session;
-import com.datastax.spark.connector.cql.CassandraConnector;
-import com.google.common.collect.ImmutableMap;
-import org.apache.spark.SparkConf;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import ratpack.exec.Blocking;
+import com.datastax.driver.core.*;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthState;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.impl.XMLResponseParser;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ratpack.exec.Promise;
 import ratpack.handling.Context;
 import ratpack.handling.Handler;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MyHandler implements Handler {
+    static Logger logger = LoggerFactory.getLogger(MyHandler.class.getName());
 
+    public static String escapeQueryChars(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // These characters are part of the query syntax and must be escaped
+            if (c == '\\' || c == '+' || c == '-' || c == '!'  || c == '(' || c == ')' || c == ':'
+                    || c == '^' || c == '[' || c == ']' || c == '\"' || c == '{' || c == '}' || c == '~'
+                    || c == '*' || c == '?' || c == '|' || c == '&'  || c == ';' || c == '/'
+                    || Character.isWhitespace(c)) {
+                sb.append('\\');
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 
-  @Override
-  public void handle(Context context) {
+    @Override
+    public void handle(Context context) {
+/*
     SparkConf conf=context.get(SparkConf.class);
 
     SparkSession spark = SparkSession
@@ -29,7 +67,7 @@ public class MyHandler implements Handler {
             .master("local[4]")
             .config(conf)
             .getOrCreate();
-
+*/
 /*
     Dataset<Row> empDF = spark
             .read()
@@ -41,14 +79,15 @@ public class MyHandler implements Handler {
             .option("driver", "oracle.jdbc.driver.OracleDriver")
             .load();
 */
-    Cluster cluster =context.get(Cluster.class);
-    Session session = cluster.connect("wiki");
-    CassandraConnector connector =  CassandraConnector.apply(spark.sparkContext().conf());
-    Session sess = connector.openSession();
-    try {
-      sess.execute("CREATE KEYSPACE test2 WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 }");
-      sess.execute("CREATE TABLE test2.words (word text PRIMARY KEY, count int)");
-    }catch (Exception e){}
+        Cluster cluster = context.get(Cluster.class);
+        Session sess = cluster.connect("artemis");
+//    CassandraConnector connector =  CassandraConnector.apply(conf);
+//    Session sess = connector.openSession();
+        try {
+            sess.execute("CREATE KEYSPACE IF NOT EXISTS test2 WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 }");
+            sess.execute("CREATE TABLE IF NOT EXISTS test2.words (word text PRIMARY KEY, count int)");
+        } catch (Exception e) {
+        }
 /*
     Map table = new HashMap();
     table.put( "table","decision_history_by_msisdn");
@@ -58,29 +97,109 @@ public class MyHandler implements Handler {
     data.show();
     data.printSchema();
 */
-/**/
-    String pSql = context.getAllPathTokens().get("sql");
+        /**/
+        String pSql = context.getAllPathTokens().get("sql");
 /*
     Dataset<Row> decHist = spark
             .read()
             .format("org.apache.spark.sql.cassandra")
 //            .options(ImmutableMap.of( "table", "decision_history_by_msisdn", "keyspace", "trex"))
-            .options(ImmutableMap.of( "table", "solr", "keyspace", "wiki", "user" , "trex", "password", "trex" ))
+            .options(ImmutableMap.of( "table", "event", "keyspace", "artemis")) //, "user" , "trex", "password", "trex" ))
             .load();
 
     decHist.createOrReplaceTempView( "decHist");
-    String sql="Select * from decHist where solr_query='title:A*' limit 2";
+    String sql="Select * from decHist where solr_query='dfptoken:d*' limit 2";
     Dataset<Row> count = spark.sql(pSql==null||pSql.isEmpty()?sql:pSql);
 */
-    String statement="Select * from wiki.solr where solr_query='title:B*' limit 2";
-    Promise<ResultSet> testCassandra = Promise.async(upstream -> {
-      ResultSetFuture resultSetFuture = session.executeAsync(pSql==null||pSql.isEmpty()?statement:pSql);
-      upstream.accept(resultSetFuture);
-    });
-    String[] xx= {"Start: " + pSql==null||pSql.isEmpty()?statement:pSql +"\n"};
 
-    testCassandra.map(resultSet -> {resultSet.all().forEach(row -> xx[0]=xx[0]+"\n"+row.toString());return xx[0]; })
-            .then(context::render);
+
+        String statement = "Select * from artemis.event where solr_query=? limit 5";
+        String solr = "";
+
+        String dfpExactId = "XX_AegiSAsLi9eQCvg6zE22dRc73lUc";
+        String dfpSmartId = "AP_S5KwmarkZ_duE1SQJX1lhVoLI";
+        String caNumber = "x";
+        String caIban = "x";
+        String caEmail = "x";
+        String address_type = "*";
+        String street = "'Am Probsthof'";
+        String street2 = "*";
+
+        solr = "{"
+                + "'q':"
+                + " '"
+                + "{!tuple "
+                + " v=\""
+                + "order_addresses.address_type:" + address_type
+                + " AND order_addresses.street:" + street2
+                + "\"}"
+//                + " OR "
+//                + "dfp_smart_id:" + dfpSmartId
+//                + " OR "
+//                + "dfp_exact_id:" + dfpExactId
+                + " '" //end q
+                + ",'sort':'total_score desc'"
+                + ", 'facet':{'field':['event_status', 'total_score', 'event_type', 'dfp_exact_id', 'dfp_smart_id', 'rmc_person_number', 'customer_account_number', 'customer_account_iban', 'customer_account_email']}"
+                +" ,'timeAllowed':3000"
+//                + ",'start':'1'"
+                + "}";
+
+        //solr=escapeQueryChars(solr);
+
+
+
+        solr="{'q':'dfp_token:*', 'sort':'total_score desc'}";
+        logger.debug("solr:" + solr);
+        PreparedStatement x = sess.prepare(statement);
+        BoundStatement bound = x.bind(solr);
+
+        Promise<ResultSet> testCassandra = Promise.async(upstream -> {
+            ResultSetFuture resultSetFuture = sess.executeAsync(bound);
+            upstream.accept(resultSetFuture);
+        });
+
+//    Promise<ResultSet> testCassandra = Promise.async(upstream -> {
+//      ResultSetFuture resultSetFuture = sess.executeAsync(pSql==null||pSql.isEmpty()?statement:pSql);
+//      upstream.accept(resultSetFuture);
+//    });
+
+/*
+        String urlString = "http://seed-solr-fheinen.cloud.scoop-gmbh.de/solr/artemis.event";
+        HttpSolrClient solrCli = new HttpSolrClient.Builder(urlString).build();
+        solrCli.setParser(new XMLResponseParser());
+
+        String username = "cassandra";
+        String password = "cassandra";
+
+        SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+
+        SolrRequest<QueryResponse> req = new QueryRequest(query);
+        req.setBasicAuthCredentials(username, password);
+
+        QueryResponse response = null;
+        try {
+            response = req.process(solrCli);
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SolrDocumentList docList = response.getResults();
+        logger.debug("Found {}", docList.getNumFound());
+
+        for (SolrDocument doc : docList) {
+            logger.debug(doc.toString());
+        }
+*/
+        String[] xx = {"Start: " + ((pSql == null || pSql.isEmpty()) ? statement : pSql) + " solr_query" + solr + "\n"};
+
+        testCassandra.map(resultSet -> {
+            resultSet.all().forEach(row -> xx[0] = xx[0] + "\n" + row.toString());
+            return xx[0];
+        })
+                .then(context::render);
 
 /*
     String[] xxx= {"Start: " + (pSql==null||pSql.isEmpty()?sql:pSql) + "\n"};
@@ -99,5 +218,5 @@ public class MyHandler implements Handler {
     testCassandra.map(resultSet -> {resultSet.all().forEach(row -> xxx[0]=xxx[0]+"\n"+row.getString("msisdn"));return xxx[0]; })
             .then(x->context.render(x));
 */
-  }
+    }
 }
